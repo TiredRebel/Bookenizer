dec20cfc0ae786ede4e0d843fecc8e36ff12a3e7
TiredRebel <dlsaus@gmail.com>
2011-05-23 08:40:12 +0300

    paging, layout switching

--- scripts/models.js
+++ scripts/models.js
@@ -1,7 +1,33 @@
 //Models
-var TopDestination=Backbone.Model.extend();
-var Statistic=Backbone.Model.extend();
-var SearchLocation=Backbone.Model.extend();
+var TopDestination = Backbone.Model.extend();
+var Statistic = Backbone.Model.extend();
+var SearchLocation = Backbone.Model.extend();
 var Language = Backbone.Model.extend();
-var SearchData=Backbone.Model.extend();
-var Hotel=Backbone.Model.extend();
+var SearchData = Backbone.Model.extend();
+var Hotel = Backbone.Model.extend();
+var SearchCriteria = {
+        language:null,
+        /*cityId:null,
+        dateFrom:null,
+        dateTo:null,
+        advDateFrom:null,
+        advDateTo:null,
+        numberOfPersons:null,
+        lodgingTypeRoom:null,
+        lodgingTypeApart:null,
+        stars2:null,
+        stars3:null,
+        stars4:null,
+        stars5:null,
+        mealsFull:null,
+        mealsHalf:null,
+        mealsNo:null,
+        pricePerNightLow:null,
+        pricePerNightHigh:null,
+        offset:null,
+        facilities:null,*/
+        searchType:"QUICK",
+        limit:null,
+        sortType:"NAME",
+        sortDirection:"ASC"
+};
