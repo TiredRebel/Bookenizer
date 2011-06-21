5b203cbb7de9eabe7185c56a837fd147311be048
TiredRebel <dlsaus@gmail.com>
2011-06-21 09:14:46 +0300

    City value validation

--- scripts/models.js
+++ scripts/models.js
@@ -3,7 +3,6 @@ var TopDestination = Backbone.Model.extend();
 var Statistic = Backbone.Model.extend();
 var SearchLocation = Backbone.Model.extend();
 var Language = Backbone.Model.extend();
-//var SearchData = Backbone.Model.extend();
 var Hotel = Backbone.Model.extend();
 var SortOrder=Backbone.Model.extend();
 var SearchCriteria = {
